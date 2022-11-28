92c5e2b48866d357f73fc84459031b41c86a1f5e
Roman I <rush7kaabap@gmail.com>
2022-11-29 00:18:41 +0500

    Lesson 01 tasks: fix1

--- 02-javascript-data-types/1-sort-strings/index.js
+++ 02-javascript-data-types/1-sort-strings/index.js
@@ -7,14 +7,14 @@
 export function sortStrings(arr, param = 'asc') {
     //let  sorted_arr = arr.sort( (a, b) => a.localeCompare(b) )
 
-    let sorted_arr = arr.sort((a, b) => {
+    let sorted_arr = arr.slice( ).sort((a, b) => {
         let srt_result1 = a.localeCompare( b );
         let srt_result2 = a.toLowerCase( ).localeCompare( b.toLowerCase( ) );
         if ( srt_result1 == srt_result2 ){
             return srt_result1;
         } else {
             return  0 - srt_result1; 
-            if ( srt_result2 == 0 ) /* {
+            /*if ( srt_result2 == 0 )  {
                 
             }
             return srt_result2; */
