0aa3f1f95432e7aaea70dca8011fad6e24f95d6d
Roman I <rush7kaabap@gmail.com>
2022-12-02 11:26:10 +0500

    Lesson 01 tasks: all tasks

--- 03-objects-arrays-intro-to-testing/1-create-getter/index.js
+++ 03-objects-arrays-intro-to-testing/1-create-getter/index.js
@@ -4,5 +4,17 @@
  * @returns {function} - function-getter which allow get value from object by set path
  */
 export function createGetter(path) {
+    return function(obj) {
+        const pathArr = path.split(".");
+
+        let curObject = obj;
+        for (let i = 0; i < pathArr.length; i++) {
+            if (curObject[pathArr[i]] === undefined){
+                return undefined;
+            };
+            curObject = curObject[pathArr[i]];
+        };
+        return curObject;    
+    };
 
 }
--- 03-objects-arrays-intro-to-testing/3-trim-symbols/index.js
+++ 03-objects-arrays-intro-to-testing/3-trim-symbols/index.js
@@ -5,5 +5,18 @@
  * @returns {string} - the new string without extra symbols according passed size
  */
 export function trimSymbols(string, size) {
+    if ( size == 0) return '';
+    if ( size == undefined ) return string;
+    if ( string == undefined ) return '';
 
+    let rezString = string.slice( 0, size );
+    const restString = string.slice( size );
+
+    for ( const char of restString){
+        let tmpString = rezString + char; 
+        if ( !tmpString.endsWith( char.repeat( size + 1 ) ) ){
+            rezString += char;
+        };
+    }; 
+    return rezString;
 }
