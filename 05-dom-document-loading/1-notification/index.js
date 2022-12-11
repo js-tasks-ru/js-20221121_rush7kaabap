e8197bc2f222c018040b36d7ea7b9c68be0ef65a
Roman I <rush7kaabap@gmail.com>
2022-12-11 21:25:45 +0500

    Lesson 5 notification task fixed

--- 05-dom-document-loading/1-notification/index.js
+++ 05-dom-document-loading/1-notification/index.js
@@ -1,34 +1,29 @@
 export default class NotificationMessage {
-    static displayed = false;
-    static innerNotif;
-    static displayTimeout; // Date()
-
-    constructor( text = '', {
-        duration = 1000,
+    //static displayed = false;
+    static lastNotif;
+    //static displayTimeout; // Date()
+    timerId;
+    constructor( message = '', {
+        duration = 2000,
         type  = 'success'
         } = {} ) {
 
-        this.text = text;
+        this.message = message;
         this.duration = duration;
         this._timeDur = (duration / 1000).toFixed(0);
-        this.mesType = type
+        this.type = type;
         
-        //this.render();
-        this.initEventListeners();
+        this.render();
     }
     
     getTemplate() {
-        return `
-        <div class="notification ${this.mesType}" 
+        return `<div class="notification ${this.type}" 
             style="--value:${this._timeDur}s">
-          <div class="timer">
-          </div>
+          <div class="timer"></div>
           <div class="inner-wrapper">
-            <div class="notification-header">
-                ${this.mesType}
-            </div>
+            <div class="notification-header">Notification</div>
             <div class="notification-body">
-                ${this.text}
+                ${this.message}
             </div>
           </div>
         </div>
@@ -39,55 +34,35 @@ export default class NotificationMessage {
         const element = document.createElement("div"); // (*)
     
         element.innerHTML = this.getTemplate();
-        //console.log(this._timeDur);
-        // NOTE: в этой строке мы избавляемся от обертки-пустышки в виде `div`
-        // который мы создали на строке (*)
+
         this.element = element.firstElementChild;
     }
 
-    show( parentElement = undefined ){
-
-        if(NotificationMessage.displayed) {
-            NotificationMessage.innerNotif.remove( );
+    show(parentElement = document.body) {
+        if(NotificationMessage.lastNotif) {
+            NotificationMessage.lastNotif.remove( );
         }
-        this.render();
-        if (parentElement == undefined ){
-            document.body.append(this.element);
-        } else {
-            parentElement.append(this.element);
-        }        
-   
-        NotificationMessage.innerNotif = this.element
-        NotificationMessage.displayed = true;   
+
+        parentElement.append(this.element);        
         
-        let outDate = new Date();
-        //console.log(+(outDate));
-        NotificationMessage.displayTimeout = +(outDate) + this.duration;   
+        this.timerId = setTimeout( ()=> {
+            this.remove();
+        }, this.duration );
 
-        //console.log(NotificationMessage.displayTimeout + " this.duration=" + this.duration);    
-        setTimeout(this.removeMethod.bind(this), this.duration); 
+        NotificationMessage.lastNotif = this;
     }
 
-    removeMethod(){
-        //NOTE: здесь должна быть функция, планирующая удалить элемент по таймауту
-        // и сбросить флаг показа
-        const timeOut = +(new Date());
-        //console.log(timeOut + " removeOnTimeOut");
-        if ( NotificationMessage.displayTimeout <= timeOut ) {
-            NotificationMessage.innerNotif.remove( );
-            NotificationMessage.displayed = false;   
+    remove(){
+        clearTimeout( this.timerId );
+        if ( this.element ){
+            this.element.remove();
         }
-    }
-
-    initEventListeners() {
-        // NOTE: в данном методе добавляем обработчики событий, если они есть
     }
 
     destroy() {
-        if (this.element != undefined){
-            this.element.remove();
-            //this.removeMethod();
-        }        
+        this.remove();
+        this.element = null;
+        NotificationMessage.lastNotif = null;
         // NOTE: удаляем обработчики событий, если они есть
-    }    
+    } 
 }
